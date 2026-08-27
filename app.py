1e20d8bb8628d0c81465b081ba35752ba7f5a583
dixmains-svg <dixmains@gmail.com>
2026-08-27 13:00:51 +0100

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 st.set_page_config(
     page_title="HAMIDOUCHE REDJDAL | CV",
-    page_icon="🚚",
+    page_icon="👨‍🎓",
     layout="wide",
     initial_sidebar_state="expanded",
 )
