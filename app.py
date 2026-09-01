d9ec544d51ed9a9e060de6c48f744696984a60f4
dixmains-svg <dixmains@gmail.com>
2026-09-01 15:45:39 +0100

    app.py

--- app.py
+++ app.py
@@ -501,7 +501,7 @@ st.sidebar.markdown(
     <div style="text-align:center; padding:10px 0px;">
         <div style="font-size:35px;">👨‍💼</div>
         <div style="font-size:18px; font-weight:800; color:white;">{nom}</div>
-        <div style="font-size:12px; color:#1F5F8B;">CURRICULUM VITAE</div>
+        <div style="font-size:12px; color:#52606d;">CURRICULUM VITAE</div>
     </div>
     """,
     unsafe_allow_html=True,
